17c0c081a0a7e5cd7c3f81c94b71f12ee99deaa3
sujonifty <sujonifty@gmail.com>
2024-03-16 05:16:15 +0600

    completed navbar section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-
+import { CgProfile } from "react-icons/cg";
 const Header = () => {
     return (
         <div>
@@ -11,37 +11,31 @@ const Header = () => {
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                                 </div>
                                 <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                                    <li><a>Item 1</a></li>
-                                    <li>
-                                        <a>Parent</a>
-                                        <ul className="p-2">
-                                            <li><a>Submenu 1</a></li>
-                                            <li><a>Submenu 2</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a>Item 3</a></li>
+                                    <li><a>Home</a></li>
+                                    <li><a>Recipes</a></li>
+                                    <li><a>About</a></li>
+                                    <li><a>Search</a></li>
                                 </ul>
                             </div>
-                            <a className="btn btn-ghost text-xl">daisyUI</a>
+                            <a className="btn btn-ghost text-xl">Recipe Calories</a>
                         </div>
                         <div className="navbar-center hidden lg:flex">
                             <ul className="menu menu-horizontal px-1">
-                                <li><a>Item 1</a></li>
-                                <li>
-                                    <details>
-                                        <summary>Parent</summary>
-                                        <ul className="p-2">
-                                            <li><a>Submenu 1</a></li>
-                                            <li><a>Submenu 2</a></li>
-                                        </ul>
-                                    </details>
-                                </li>
-                                <li><a>Item 3</a></li>
+                                <li><a>Home</a></li>
+                                <li><a>Recipes</a></li>
+                                <li><a>About</a></li>
+                                <li><a>Search</a></li>
                             </ul>
                         </div>
-                        <div className="navbar-end">
-                            <a className="btn">Button</a>
+                        <div className=" navbar-end  justify-center gap-2 hidden lg:flex ">
+                            <label className=" input input-bordered flex items-center gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" /></svg>
+                                <input type="text" className="grow" placeholder="Search" />
+
+                            </label>
+                            <a className=" p-3 rounded-full bg-green-400"><CgProfile /></a>
                         </div>
+                        <a className="ml-28 md:hidden lg:hidden p-3 rounded-full bg-green-400"><CgProfile /></a>
                     </div>
                 </nav>
             </section>
